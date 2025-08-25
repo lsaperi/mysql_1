f9fca56a822ccfc100990031338b817cb402fc31
lsaperi <lsaperi>
2025-08-25 17:16:29 +0000

    asdf; TaskID: asdf

---
+++ TABLEs/Creation-Scripts/address.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `address` (
+  `idaddress` int NOT NULL,
+  `street` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`idaddress`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
